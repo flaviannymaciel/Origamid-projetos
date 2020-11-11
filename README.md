ef7a03e0c3feb45060ca1e1f8322668c4f4bff8e
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-11-11 17:06:36 -0300

    feature accordionList

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Origamid-projetos
-Aprenda do zero Web Design e comece a sua carreira de UX / UI Designer e Desenvolvedor Front end. Mais de 975 aulas divididas em 19 cursos.
+**Javascript**
+Neste curso você irá aprender tudo que é necessário para dominar o JavaScript. O curso vai do básico ao avançado e ao final dele você terá uma compreensão total da linguagem, dos seus principais métodos e da sua sintaxe.
+
+O foco é no aprendizado da linguagem, mas o principal local de aplicação será na web, através de manipulações do DOM e requisições via Fetch API.
+
+O código ensinado utiliza todas as novidades das novas versões de JavaScript, como arrow functions, promises e mais.
--- projeto-animais-fantasticos/script.js
+++ projeto-animais-fantasticos/script.js
@@ -1,17 +1,41 @@
-const tabMenu = document.querySelectorAll('.jsTabMenu li');
-const tabContent = document.querySelectorAll('.jsTabContent section');
-
-if (tabMenu.length && tabContent.length) {
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
+//Navegação por tab
+function initTabNav() {
+  // adicionando funcao para isolar em um escopo e não gerar conflito
+  const tabMenu = document.querySelectorAll('.jsTabMenu li');
+  const tabContent = document.querySelectorAll('.jsTabContent section');
+  // padrão default com o primeiro conteúdo ativo
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
+    // função para remover ativo existente e add em novo elemento
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+      });
+      tabContent[index].classList.add('ativo');
+    }
+    // relacionar o clique na imagem com o conteúdo
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add('ativo');
   }
-
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
-    });
-  });
 }
+initTabNav();
+
+//Accordion List
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo'
+
+  function activeAccordion() {
+    this.classList.toggle(activeClass);
+    this.nextElementSibling.classList.toggle(activeClass);
+  }
+  accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+  });
+
+  addAtivo();
+}
+initAccordion();
