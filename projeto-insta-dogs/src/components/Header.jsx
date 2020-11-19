e0b46ec5b2c11b391a85369f41c78425ac25ed40
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-11-19 00:51:43 -0300

    Header

--- projeto-insta-dogs/src/components/Header.jsx
+++ projeto-insta-dogs/src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Header.modules.css';
+import styles from './Header.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login/Criar</Link>
+    <header className={styles.header}>
+      <nav className={ `${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs-Home"><Dogs /></Link>
+        <Link className={styles.login} to="/login">Login / Criar</Link>
       </nav>
-    </div>
+    </header>
   )
 }
 
