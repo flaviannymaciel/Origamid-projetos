e93f094af132afb9aa1681e6205c510fb2a3a9f0
flaviannymaciel <flaviannymaciel@gmail.com>
2021-03-22 21:19:13 -0300

    userContext 🚧

--- projeto-app-dogs/dogs/src/App.js
+++ projeto-app-dogs/dogs/src/App.js
@@ -5,17 +5,20 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import { UserContext } from './UserContext';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserContext>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserContext>
       </BrowserRouter>
     </div>
   );
