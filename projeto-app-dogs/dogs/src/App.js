67fb2d1c43f01f4777037b642886d75c5c962a0a
flaviannymaciel <flaviannymaciel@gmail.com>
2021-03-22 22:47:31 -0300

    validate Token 🚧

--- projeto-app-dogs/dogs/src/App.js
+++ projeto-app-dogs/dogs/src/App.js
@@ -5,20 +5,20 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
-import { UserContext } from './UserContext';
+import { UserStorage } from './UserContext';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <UserContext>
+        <UserStorage>
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
           </Routes>
           <Footer />
-        </UserContext>
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
